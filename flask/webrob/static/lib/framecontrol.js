56c7fb8b73f1255909dd058a07c47d3da11bbf20
Moritz Horstmann <dev@peterzweg.at>
2018-12-20 14:37:54 +0100

    Allow dynamic update of frames and menus

--- flask/webrob/static/lib/framecontrol.js
+++ flask/webrob/static/lib/framecontrol.js
@@ -9,15 +9,67 @@ function FrameControl(options){
     this.menu = options.menu;
 
     // User interface names (e.g., editor, memory replay, ...)
-    var user_interfaces = options.user_interfaces || [];
-    var user_interfaces_flat = options.user_interfaces_flat || [];
+    this.common_user_interfaces = options.common_user_interfaces || [];
+
+    this.webclient_user_interfaces = [];
+
+    this.user_interfaces = [];
+
+    this.user_interfaces_flat = [];
+
+    this.pageOverlayDisabled = false;
 
     // Query parameters encoded in URL
     // E.g., localhost/#foo&bar=1 yields in:
     //    URL_QUERY = {foo: undefined, bar: 1}
     var urlQuery = {};
 
-    this.pageOverlayDisabled = false;
+    this.init = function() {
+        that.update_webclient_frames([]);
+        createFrames(that.common_user_interfaces);
+    }
+
+    this.update_webclient_frames = function(webclient_user_interfaces) {
+        for(var i in that.webclient_user_interfaces) {
+            var id = that.webclient_user_interfaces[i].id + '-frame';
+            var element = document.getElementById(id);
+            element.parentNode.removeChild(element);
+        }
+        that.user_interfaces = webclient_user_interfaces.concat(that.common_user_interfaces);
+        that.webclient_user_interfaces = webclient_user_interfaces;
+        that.update_flat();
+        createFrames(webclient_user_interfaces);
+
+    }
+
+    that.update_flat = function() {
+        that.user_interfaces_flat = [];
+        for(var i in that.user_interfaces) {
+            var elem = that.user_interfaces[i];
+            var ifaces = elem.interfaces;
+            if(!ifaces) ifaces = [elem];
+            for(var j in ifaces) {
+                that.user_interfaces_flat.push(ifaces[j]);
+            }
+        };
+    }
+
+    function createFrames(interfaces) {
+        // Declare page frames: Each user interface of openEASE is
+        // put into an iframe that is child of #page
+        for(var i in interfaces) {
+            var elem = interfaces[i];
+            var frame = document.createElement("iframe");
+            frame.id = elem.id+'-frame';
+            frame.className = 'content-frame';
+            if(elem.interfaces && elem.interfaces[0].src)
+                frame.src = elem.interfaces[0].src;
+            else if(elem.src)
+                frame.src = elem.src;
+            else continue;
+            document.getElementById('page').appendChild(frame);
+        };
+    }
 
     ///////////////////////////////
     //////////// URL Location
@@ -47,7 +99,7 @@ function FrameControl(options){
 
     this.updateLocation = function() {
       updateQueryString();
-      showFrame(getActiveInterfaceName());
+      that.showFrame(that.getActiveInterfaceName());
       // update episode selection from URL query
       // e.g., https://data.openease.org/#kb?category=foo?episode=bar
       if(urlQuery['category'] && urlQuery['episode']) {
@@ -99,17 +151,17 @@ function FrameControl(options){
     //////////// Frames
     ///////////////////////////////
 
-    function showFrame(iface_name) {
-        var frame_name = getInterfaceFrameName(iface_name);
+    this.showFrame = function (iface_name) {
+        var frame_name = that.getInterfaceFrameName(iface_name);
         // Hide inactive frames
-        for(var i in user_interfaces) {
-            if(user_interfaces[i].id == frame_name) continue;
-            $("#"+user_interfaces[i].id+"-frame").hide();
-            $("#"+user_interfaces[i].id+"-frame").removeClass("selected-frame");
-            $("#"+user_interfaces[i].id+"-menu").removeClass("selected-menu");
+        for(var i in that.user_interfaces) {
+            if(that.user_interfaces[i].id == frame_name) continue;
+            $("#"+that.user_interfaces[i].id+"-frame").hide();
+            $("#"+that.user_interfaces[i].id+"-frame").removeClass("selected-frame");
+            $("#"+that.user_interfaces[i].id+"-menu").removeClass("selected-menu");
         }
 
-        var new_src = getInterfaceSrc(iface_name);
+        var new_src = that.getInterfaceSrc(iface_name);
         var frame = document.getElementById(frame_name+"-frame");
         var old_src = frame.src;
         if(!old_src.endsWith(new_src)) {
@@ -127,15 +179,15 @@ function FrameControl(options){
     };
 
     this.getActiveFrame = function() {
-        var frame = document.getElementById(getActiveFrameName()+"-frame");
+        var frame = document.getElementById(that.getActiveFrameName()+"-frame");
         if(frame) return frame.contentWindow;
         else return window;
         //else return undefined;
     };
 
-    function getInterfaceFrameName(iface) {
-        for(var i in user_interfaces) {
-            var elem = user_interfaces[i];
+    this.getInterfaceFrameName = function(iface) {
+        for(var i in that.user_interfaces) {
+            var elem = that.user_interfaces[i];
             if(elem.id == iface) return elem.id;
             for(var j in elem.interfaces) {
                 if(elem.interfaces[j].id == iface) return elem.id;
@@ -143,9 +195,9 @@ function FrameControl(options){
         }
     };
 
-    function getInterfaceSrc(iface) {
-        for(var i in user_interfaces) {
-            var elem = user_interfaces[i];
+    this.getInterfaceSrc = function (iface) {
+        for(var i in that.user_interfaces) {
+            var elem = that.user_interfaces[i];
             if(elem.id == iface) return elem.src;
             for(var j in elem.interfaces) {
                 if(elem.interfaces[j].id == iface) return elem.interfaces[j].src;
@@ -153,28 +205,28 @@ function FrameControl(options){
         }
     };
 
-    function getActiveFrameName() {
-      return getInterfaceFrameName(getActiveInterfaceName());
+    this.getActiveFrameName = function () {
+      return that.getInterfaceFrameName(that.getActiveInterfaceName());
     };
 
-    function getActiveInterfaceName() {
-      for(var i in user_interfaces_flat) {
-        if(urlQuery[user_interfaces_flat[i].id]) return user_interfaces_flat[i].id;
+    this.getActiveInterfaceName = function () {
+      for(var i in that.user_interfaces_flat) {
+        if(urlQuery[that.user_interfaces_flat[i].id]) return that.user_interfaces_flat[i].id;
       }
       return "kb";
     };
 
     this.on_register_nodes_all = function() {
-        for(var i in user_interfaces) {
-          var frame = document.getElementById(user_interfaces[i].id+"-frame");
+        for(var i in that.user_interfaces) {
+          var frame = document.getElementById(that.user_interfaces[i].id+"-frame");
           if(frame && frame.contentWindow && frame.contentWindow.on_register_nodes)
               frame.contentWindow.on_register_nodes();
       }
     }
 
     this.on_episode_selected_all = function(library) {
-        for (var i in user_interfaces) {
-            var frame = document.getElementById(user_interfaces[i].id + "-frame");
+        for (var i in that.user_interfaces) {
+            var frame = document.getElementById(that.user_interfaces[i].id + "-frame");
             if (frame && frame.contentWindow.on_episode_selected)
                 frame.contentWindow.on_episode_selected(library);
         }
